7b5e5fb170d74b969d6d45640cdfa8663e910e00
komal030 <96829750+komal030@users.noreply.github.com>
2023-08-22 15:22:13 +0530

    q1.c

--- OS Lab/WEEK 3/q1.c
+++ OS Lab/WEEK 3/q1.c
@@ -4,22 +4,20 @@
 #include <string.h>
 #include <sys/wait.h>
 #include <sys/types.h>
+
 int main() {
   pid_t pid = fork();
   char message[50];
-  int n, status;
+  int status;
   if (pid < 0) {
     printf("Error encountered");
     exit(-1);
   } else if (pid == 0) {
     strcpy(message, "I am child process");
-    n = 5;
   } else {
     wait( & status);
     printf("Child process returned: %d\n", status);
     strcpy(message, "I am parent process");
-    n = 3;
   }
-  for (; n > 0; n--)
-    printf("%s\n", message);
+  printf("%s\n", message);
 }
