8625d0eec8ae69bca2bc561a54eed3d9c05c71d9
komal030 <96829750+komal030@users.noreply.github.com>
2023-08-25 09:54:00 +0530

    q2

---
+++ CN LAB/WEEK 3/q2.c
@@ -0,0 +1,11 @@
+//write these commands on the terminal
+
+telnet www.google.com 80
+Trying 142.250.183.196...
+Connected to www.google.com.
+Escape character is '^]'.
+
+GET / HTTP/1.1
+Host: www.google.com
+Connection: close
+//press double enter
