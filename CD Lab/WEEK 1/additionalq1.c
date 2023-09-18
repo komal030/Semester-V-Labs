7d7029d960054a52c45840b726b8a16508264fb7
komal030 <96829750+komal030@users.noreply.github.com>
2023-09-18 19:15:38 +0530

    additionalq1

--- CD Lab/WEEK 1/additionalq1.c
+++ CD Lab/WEEK 1/additionalq1.c
@@ -1,37 +1,40 @@
 #include <stdio.h>
 #include <stdlib.h>
-int main() {
-  FILE *f1;
-  char fname[100], c;
-  printf("Enter name of file to read\n");
-  scanf("%s", fname);
-  f1 = fopen(fname, "r");
-  if (f1 == NULL) {
-    printf("Cannot open file\n");
-    exit(1);
-  }
-  int blankline = 0, semi = 0, blankspace = 0;
-  c = fgetc(f1);
-  while (c != EOF) {
-    if (c == ' ')
-      blankspace++;
-    if (c == ';') {
-      c = fgetc(f1);
-      if (c == '\n') {
-        semi++;
-        fseek(f1, -1, SEEK_CUR);
-      }
-    }
-    if (c == '\n') {
-      c = fgetc(f1);
-      if (c == '\n') {
-        blankline++;
-      }
-    }
-    c=fgetc(c);
-  }
- 
- printf("Number of sentences ending with semi colon: %d\nNumber of blank sentences: %d\nNumber of blank spaces: %d", semi, blankline, blankspace);
-  fclose(f1);
-  return 1;
+#include <string.h>
+int main()
+{
+FILE *f1;
+char filename[]="input.c";
+f1=fopen(filename,"r");
+if(f1==NULL)
+{
+printf("Error");
+exit(0);
+}
+char c=fgetc(f1);
+int blankspaces=0;
+int semicolon=0;
+int blanklines=0;
+char s1[100];
+while(fgets(s1,100,f1)!=NULL)
+{
+if(strcmp(s1,"\n")==0)
+blanklines++;
+}
+rewind(f1);
+while(c!=EOF)
+{
+if(c==' ')
+blankspaces++;
+if(c==';')
+{
+c=fgetc(f1);
+if(c=='\n')
+semicolon++;
+fseek(f1,-1,SEEK_CUR);
+}
+c=fgetc(f1);
+}
+printf("Semicolons %d\nBlanklines %d\nBlankSpaces %d\n",semicolon,blanklines,blankspaces);
+fclose(f1);
 }
